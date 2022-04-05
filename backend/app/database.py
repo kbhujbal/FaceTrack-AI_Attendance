e2ee3a8c29fdac6b62db267808fc4b19c6004a12
kbhujbal <kbhujbal@usc.edu>
2022-04-05 12:00:00 -0700

    backend

---
+++ backend/app/database.py
@@ -0,0 +1,32 @@
+"""
+Database connection and session management
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from typing import Generator
+from app.config import settings
+
+# Create engine
+engine = create_engine(
+    str(settings.DATABASE_URL),
+    pool_size=settings.DB_POOL_SIZE,
+    max_overflow=settings.DB_MAX_OVERFLOW,
+    pool_pre_ping=True,
+    echo=settings.DEBUG
+)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
+
+
+def get_db() -> Generator[Session, None, None]:
+    """Dependency for getting database session"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
