bb7fe1c2aee897321f422972fcba03edf3609f4c
kbhujbal <kbhujbal@usc.edu>
2022-04-15 12:00:00 -0700

    backend

---
+++ backend/app/main.py
@@ -0,0 +1,81 @@
+"""
+FastAPI Application - Face Recognition Attendance System Backend
+"""
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from loguru import logger
+import time
+
+from app.config import settings
+from app.api.v1 import schedule, attendance, heartbeat
+
+# Initialize FastAPI app
+app = FastAPI(
+    title=settings.APP_NAME,
+    version=settings.APP_VERSION,
+    docs_url="/docs",
+    redoc_url="/redoc"
+)
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Request timing middleware
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+
+# Exception handlers
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.error(f"Unhandled exception: {exc}", exc_info=True)
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal server error"}
+    )
+
+
+# Include routers
+app.include_router(schedule.router, prefix=settings.API_PREFIX, tags=["schedule"])
+app.include_router(attendance.router, prefix=settings.API_PREFIX, tags=["attendance"])
+app.include_router(heartbeat.router, prefix=settings.API_PREFIX, tags=["heartbeat"])
+
+
+@app.get("/")
+async def root():
+    """API root endpoint"""
+    return {
+        "name": settings.APP_NAME,
+        "version": settings.APP_VERSION,
+        "status": "operational"
+    }
+
+
+@app.get("/health")
+async def health_check():
+    """Health check endpoint for load balancers"""
+    return {"status": "healthy"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "app.main:app",
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=settings.DEBUG,
+        workers=settings.WORKERS if not settings.DEBUG else 1
+    )
